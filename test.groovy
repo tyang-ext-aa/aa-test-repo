8247c265833608c2501aa6813f3541a83b862023
tyang-ext-aa <tyang-ext@appannie.com>
2018-04-18 11:51:00 +0800

    asd

--- test.groovy
+++ test.groovy
@@ -4,6 +4,7 @@ node('Yang Tao'){
 	checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${env.BRANCH_NAME}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 30]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/tyang-ext-aa/aa-test-repo.git']]]
 
     stage('Build'){
+    	echo "1"
     	echo env.BRANCH_NAME
     	echo "hi" 
     	echo "2"
