b769bae4fdee26eff47c28ebf3ddf94ff34ecdd3
tyang-ext-aa <tyang-ext@appannie.com>
2018-04-19 09:54:49 +0800

    09-54

--- test.groovy
+++ test.groovy
@@ -1,15 +1,10 @@
 node('Yang Tao'){
- 
-
 	checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${env.BRANCH_NAME}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 30]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/tyang-ext-aa/aa-test-repo.git']]]
- 
     stage('Build'){
-    	echo "1"
-    	echo env.BRANCH_NAME
-    	echo "hi" 
-    	echo "2"
-      	sh "ls -a"
-      	sh "sh test.sh"
-      	echo "MIRROR_REPOS_PATH :" + env.MIRROR_REPOS_PATH
+      echo "echo env.BRANCH_NAME " + env.BRANCH_NAME
+      echo "  echo env.JOB_NAME " + env.JOB_NAME
+      sh "ls -a"
+      sh "sh test.sh"
+      echo "MIRROR_REPOS_PATH :" + env.MIRROR_REPOS_PATH
     }
 }
