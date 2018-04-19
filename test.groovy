5ce5d16f859d0f3e9c5ea3eeb860aafa659ffca1
tyang-ext-aa <tyang-ext@appannie.com>
2018-04-19 09:59:35 +0800

    09-59

--- test.groovy
+++ test.groovy
@@ -1,5 +1,5 @@
 node('Yang Tao'){
-	checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${env.BRANCH_NAME}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 30]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/tyang-ext-aa/aa-test-repo.git']]]
+	checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: env.BRANCH_NAME]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 30]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/tyang-ext-aa/aa-test-repo.git']]]
     stage('Build'){
       echo "echo env.BRANCH_NAME " + env.BRANCH_NAME
       echo "  echo env.JOB_NAME " + env.JOB_NAME
